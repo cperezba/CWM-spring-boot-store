46c8a89eb6f7d858cc10872930103afffa7a1847
Carlos Perez <ca.perez.barr.dev@gmail.com>
2026-04-03 18:34:12 -0500

    Initialize Spring Boot project with Maven Wrapper and basic structure, including `.gitignore`, application properties, and a simple `HomeController`.

---
+++ src/test/java/org/codewithmosh/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codewithmosh.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
